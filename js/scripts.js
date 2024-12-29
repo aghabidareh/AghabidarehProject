a609d31ba7f32d43880a5c77b4d489b466aee8f5
aghabidareh <aghabidareh@gmail.com>
2024-12-29 14:15:04 +0330

    being-ready-why-and-about-me-page

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,3 @@ function scrollToTop() {
 function location_href(link){
     return window.location.href = link;
 }
-
-const backToTopButton = document.querySelector('.back-to-top');
-
-backToTopButton.addEventListener('mouseover', () => {
-    backToTopButton.classList.add('rotating');
-});
-
-backToTopButton.addEventListener('animationend', () => {
-    backToTopButton.classList.remove('rotating');
-});
-
-
-
