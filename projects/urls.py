991ef3edcb2dd0bd924144541d566fdc0e171a00
aghabidareh <aghabidareh@gmail.com>
2025-01-16 22:09:27 -0800

    define the urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,11 @@
-from django.contrib import admin
+from django.urls import path
+from projects.views import *
 
 urlpatterns = [
-
+    path('' , index , name='index'),
+    path('about-me' , about_me , name='about-me'),
+    path('why' , why , name='why'),
+    path('text-simialrity' , text_simialrity , name='text-simialrity'),
+    path('category-detection' , category_detection , name='category-detection'),
+    path('author' , author , name='author'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    pass
+
+def about_me(request):
+    pass
+
+def why(request):
+    pass
+
+def text_simialrity(request):
+    pass
+
+def category_detection(request):
+    pass
+
+def author(request):
+    pass
