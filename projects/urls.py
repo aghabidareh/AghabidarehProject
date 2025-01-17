8c8e2d79aa4ea8c5d98aadef3fe830175b9b6d6a
aghabidareh <aghabidareh@gmail.com>
2025-01-16 22:25:34 -0800

    create the base of the pages

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('' , index , name='index'),
     path('about-me' , about_me , name='about-me'),
     path('why' , why , name='why'),
-    path('text-simialrity' , text_similarity , name='text-simialrity'),
+    path('text-simialrity' , text_similarity , name='text-similarity'),
     path('category-detection' , category_detection , name='category-detection'),
     path('author' , author , name='author'),
 ]
