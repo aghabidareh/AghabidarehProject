105a6addcd89bd249a09babd23e20994eafe17da
aghabidareh <aghabidareh@gmail.com>
2025-02-10 05:40:49 -0800

    add decorators

--- projects/views.py
+++ projects/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET, require_http_methods
+
 from projects.modules.category_detection.predict import predict
 
+@require_GET
 def index(request):
     return render(request , 'projects/index.html')
 
+@require_GET
 def about_me(request):
     return render(request , 'projects/about-me.html')
 
+@require_GET
 def why(request):
     return render(request , 'projects/why.html')
 
+@require_http_methods(['GET', 'POST'])
 def text_similarity(request):
     return render(request , 'projects/text-similarity.html')
 
+@require_http_methods(['GET', 'POST'])
 def category_detection(request):
     if request.method == "POST":
         text = request.POST['text']
@@ -22,5 +29,6 @@ def category_detection(request):
 
     return render(request , 'projects/category-detection.html')
 
+@require_http_methods(['GET', 'POST'])
 def poet(request):
     return render(request, 'projects/poet.html')
