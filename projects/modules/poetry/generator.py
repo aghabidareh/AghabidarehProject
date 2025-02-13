2ce6a91473d26eb1d98516e5f5af557db6b1569e
aghabidareh <aghabidareh@gmail.com>
2025-02-13 14:13:57 -0800

    prepare-files-for-poetry-project

---
+++ projects/modules/poetry/generator.py
@@ -0,0 +1,9 @@
+from projects.modules.poetry.loader import *
+
+def generator(input_text):
+    model.eval()
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1, no_repeat_ngram_size=2, top_k=50,
+                            top_p=0.95, temperature=0.7)
+    generated_poem = tokenizer.decode(output[0], skip_special_tokens=True)
+    return generated_poem.replace("<sep>" , '\n').replace('<|startoftext|>' , '')
