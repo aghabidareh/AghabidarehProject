e4ce0f7fa6febf0e8642ad730bc76e5b297eb5e4
aghabidareh <aghabidareh@gmail.com>
2025-02-14 08:49:35 -0800

    complete-poetry-fine-tuned

--- projects/modules/poetry/loader.py
+++ projects/modules/poetry/loader.py
@@ -1,5 +1,5 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_path = "./gpt2-fa-poetry-finetuned"
+model_path = "projects\modules\poetry\gpt2-fa-poetry-finetuned"
 model = GPT2LMHeadModel.from_pretrained(model_path)
 tokenizer = GPT2Tokenizer.from_pretrained(model_path)
