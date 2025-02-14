df357b76791683c523487efa38ee2a375674a84b
aghabidareh <aghabidareh@gmail.com>
2025-02-14 09:14:34 -0800

    new-settings-to-generate-poems

--- projects/modules/poetry/generator.py
+++ projects/modules/poetry/generator.py
@@ -4,6 +4,6 @@ def generator(input_text):
     model.eval()
     input_ids = tokenizer.encode(input_text, return_tensors='pt')
     output = model.generate(input_ids, max_length=100, no_repeat_ngram_size=2,
-                            temperature=0.9 , num_beams=7 , early_stopping=False)
+                            temperature=0.6 , num_beams=7 , early_stopping=False)
     generated_poem = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_poem.replace("<sep>" , '\n').replace('<|startoftext|>' , '')
--- projects/modules/poetry/loader.py
+++ projects/modules/poetry/loader.py
@@ -1,5 +1,5 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_name = "projects/modules/poetry/gpt2-fa-poetry-finetuned"
+model_name = "HooshvareLab/gpt2-fa-poetry"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
