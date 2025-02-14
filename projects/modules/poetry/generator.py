8e2642fff3eae003b853a16245b381f49305a261
aghabidareh <aghabidareh@gmail.com>
2025-02-14 08:59:56 -0800

    new-settings-to-generate-poems

--- projects/modules/poetry/generator.py
+++ projects/modules/poetry/generator.py
@@ -3,7 +3,7 @@ from projects.modules.poetry.loader import *
 def generator(input_text):
     model.eval()
     input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    output = model.generate(input_ids, max_length=100, num_return_sequences=1, no_repeat_ngram_size=2, top_k=90,
-                            top_p=0.95, temperature=0.9)
+    output = model.generate(input_ids, max_length=100, no_repeat_ngram_size=2,
+                            temperature=0.9 , num_beams=7 , early_stopping=False)
     generated_poem = tokenizer.decode(output[0], skip_special_tokens=True)
     return generated_poem.replace("<sep>" , '\n').replace('<|startoftext|>' , '')
--- projects/modules/poetry/loader.py
+++ projects/modules/poetry/loader.py
@@ -1,5 +1,5 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_path = "projects\modules\poetry\gpt2-fa-poetry-finetuned"
-model = GPT2LMHeadModel.from_pretrained(model_path)
-tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+model_name = "projects/modules/poetry/gpt2-fa-poetry-finetuned"
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
