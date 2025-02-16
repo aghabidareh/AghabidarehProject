479ef980c82bf0b4cedcc49cf80b9e03419d8bb1
aghabidareh <aghabidareh@gmail.com>
2025-02-16 05:44:36 -0800

    make-the-poetry-generator-ok

--- projects/modules/poetry/generator.py
+++ projects/modules/poetry/generator.py
@@ -1,7 +1,6 @@
 from projects.modules.poetry.loader import *
 
 def generator(input_text):
-    model.eval()
     input_ids = tokenizer.encode(input_text, return_tensors='pt')
     output = model.generate(input_ids, max_length=100, no_repeat_ngram_size=2,
                             temperature=0.6 , num_beams=7 , early_stopping=False)
