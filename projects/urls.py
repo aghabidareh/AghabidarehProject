db5b083401580cfed52d6f27fabe943bf2cc9624
aghabidareh <aghabidareh@gmail.com>
2025-02-25 02:13:53 -0800

    correcting-urls

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('' , index , name='index'),
     path('about-me' , about_me , name='about-me'),
     path('why' , why , name='why'),
-    path('text-similarity' , text_similarity , name='text-similarity'),
-    path('category_detection' , category_detection , name='category_detection'),
-    path('poet' , poet , name='poet'),
+    path('project/text-similarity' , text_similarity , name='text-similarity'),
+    path('project/category_detection' , category_detection , name='category_detection'),
+    path('project/poet' , poet , name='poet'),
 ]
