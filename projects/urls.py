625192edf6a57556154ae96a7480d15f7b30ee80
aghabidareh <aghabidareh@gmail.com>
2025-02-26 23:54:26 -0800

    add-necessary-comments

--- projects/urls.py
+++ projects/urls.py
@@ -2,10 +2,21 @@ from django.urls import path
 from projects.views import index , about_me , why , text_similarity , category_detection , poet
 
 urlpatterns = [
+    # index page
     path('' , index , name='index'),
+
+    # about me page
     path('about-me' , about_me , name='about-me'),
+
+    # why page
     path('why' , why , name='why'),
+
+    # similarity between two textx project page
     path('project/text-similarity' , text_similarity , name='text-similarity'),
+
+    # persian text category detection project page
     path('project/category_detection' , category_detection , name='category_detection'),
+
+    # poetry project page
     path('project/poet' , poet , name='poet'),
 ]
