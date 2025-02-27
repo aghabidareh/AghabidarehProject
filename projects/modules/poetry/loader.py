eb796fd0651efd36bdf0c5be5059b0fa95b8868d
aghabidareh <aghabidareh@gmail.com>
2025-02-27 00:59:55 -0800

    add-necessary-comments

--- projects/modules/poetry/loader.py
+++ projects/modules/poetry/loader.py
@@ -1,5 +1,10 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+"""
+Loading the model and tokenizer to create the poem
+using trained model and tokenizer
+"""
+
 model_name = "projects/modules/poetry/gpt2-fa-poetry-finetuned"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
