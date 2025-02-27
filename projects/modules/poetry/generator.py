b61002f01e363b94a638120b1cb49cdea0c79abc
aghabidareh <aghabidareh@gmail.com>
2025-02-27 01:01:06 -0800

    add-necessary-comments

--- projects/modules/poetry/generator.py
+++ projects/modules/poetry/generator.py
@@ -1,6 +1,11 @@
 from projects.modules.poetry.loader import *
 
 def generator(input_text):
+    """
+    prepare the input text
+    create the poem based on the input text
+    using model and tokenizer which are loaded
+    """
     input_ids = tokenizer.encode(input_text, return_tensors='pt')
     output = model.generate(input_ids, max_length=100, no_repeat_ngram_size=2,
                             temperature=0.6 , num_beams=7 , early_stopping=False)
