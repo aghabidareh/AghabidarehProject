dafa3236d9fa39bf1a46709e149304a8120b72d7
aghabidareh <aghabidareh@gmail.com>
2025-02-27 22:43:53 -0800

    optimized-paths

--- projects/modules/poetry/loader.py
+++ projects/modules/poetry/loader.py
@@ -1,3 +1,4 @@
+import os
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 """
@@ -5,6 +6,9 @@ Loading the model and tokenizer to create the poem
 using trained model and tokenizer
 """
 
-model_name = "projects/modules/poetry/gpt2-fa-poetry-finetuned"
+current_directory = os.path.dirname(os.path.abspath(__file__))
+model_name_directory = os.path.join(current_directory, 'gpt2-fa-poetry-finetuned')
+
+model_name = model_name_directory
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
