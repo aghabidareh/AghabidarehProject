77780f183190d80b991a5774b8590f5f1b6589cb
aghabidareh <aghabidareh@gmail.com>
2025-02-27 22:42:41 -0800

    optimized-paths

--- projects/modules/category_detection/loader.py
+++ projects/modules/category_detection/loader.py
@@ -1,3 +1,4 @@
+import os
 from joblib import load
 import nltk
 
@@ -8,27 +9,37 @@ using with and joblib
 also stopword from nltk
 """
 
-with open('projects\modules\category_detection\stopwords.txt' , 'r' , encoding='utf-8') as stopwords_file:
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+stopwords_directory = os.path.join(current_directory , 'stopwords.txt')
+model_directory = os.path.join(current_directory , 'model.h5')
+normalizer_directory = os.path.join(current_directory , 'normalizer.h5')
+stemmer_lemmatizer_directory = os.path.join(current_directory , 'stemmer.h5')
+tokenizer_directory = os.path.join(current_directory , 'tokenizer.h5')
+vectorizer_directory = os.path.join(current_directory , 'vectorizer.h5')
+label_encoder_directory = os.path.join(current_directory , 'encoder.h5')
+
+with open(stopwords_directory , 'r' , encoding='utf-8') as stopwords_file:
     stopwords = stopwords_file.readlines()
 stopwords = [line.replace('\n' , '') for line in stopwords]
 
 nltk_stopwords = nltk.corpus.stopwords.words('english')
 stopwords.extend(nltk_stopwords)
 
-with open('projects\modules\category_detection\model.h5' , 'rb') as f:
+with open(model_directory , 'rb') as f:
     model = load(f)
 
-with open('projects\modules\category_detection\\normalizer.h5' , 'rb') as f:
+with open(normalizer_directory , 'rb') as f:
     normalizer = load(f)
 
-with open('projects\modules\category_detection\stemmer.h5' , 'rb') as f:
+with open(stemmer_lemmatizer_directory , 'rb') as f:
     stemmer_lemmatizer = load(f)
 
-with open('projects\modules\category_detection\\tokenizer.h5' , 'rb') as f:
+with open(tokenizer_directory , 'rb') as f:
     tokenizer = load(f)
 
-with open('projects\modules\category_detection\\vectorizer.h5' , 'rb') as f:
+with open(vectorizer_directory , 'rb') as f:
     vectorizer = load(f)
 
-with open('projects\modules\category_detection\encoder.h5' , 'rb') as f:
+with open(label_encoder_directory , 'rb') as f:
     label_encoder = load(f)
