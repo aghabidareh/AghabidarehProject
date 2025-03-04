7f8474d6c86b1e64b770d35863c447fb27181a0a
aghabidareh <aghabidareh@gmail.com>
2025-03-04 08:43:27 -0800

    add-explanation-for-every-record

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,6 @@ class News(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} : {self.description[:10]}"
