fa11ca57b20d8e2594b995e4db4ff92600d3307d
aghabidareh <aghabidareh@gmail.com>
2025-03-04 08:44:41 -0800

    new-field-is_published

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    is_published = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
