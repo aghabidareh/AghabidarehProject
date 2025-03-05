5fecf54047ce545e4d0f5355692c8fbb79926cb0
aghabidareh <aghabidareh@gmail.com>
2025-03-05 07:31:39 -0800

    change-max_length-of-title-field

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=32)
     description = models.TextField()
     is_published = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
