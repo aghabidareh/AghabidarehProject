a7e2bb12309286db3e45559d26d3cbc85ed0033b
aghabidareh <aghabidareh@gmail.com>
2025-03-05 07:56:58 -0800

    add-an-action

--- projects/admin.py
+++ projects/admin.py
@@ -10,3 +10,7 @@ class NewsAdmin(admin.ModelAdmin):
     list_display_links = ('title',)
     search_fields = ('title','description')
     list_per_page = 50
+    actions = ('publish',)
+
+    def publish(self, request, queryset):
+        queryset.update(is_published=True)
