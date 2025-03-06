685bc85d28063486915f6d4ad35211def61c9427
aghabidareh <aghabidareh@gmail.com>
2025-03-06 01:59:08 -0800

    new-cleaning

--- projects/views.py
+++ projects/views.py
@@ -9,46 +9,37 @@ from projects.utils.utils import similarity_detection, category_serializer, poet
 # requirement
 @require_GET
 def index(request):
-    # render the html
     return render(request , 'projects/index.html')
 
 # requirement
 @require_GET
 def about_me(request):
-    # render the html
     return render(request , 'projects/about-me.html')
 
 # requirement
 @require_GET
 def why(request):
-    # render the html
     return render(request , 'projects/why.html')
 
 # requirement
 @require_http_methods(['GET', 'POST'])
 def text_similarity(request):
     if request.method == 'POST':
-        # response
         return similarity_detection(request)
-    # render the html
     return render(request , 'projects/text-similarity.html')
 
 # requirement
 @require_http_methods(['GET', 'POST'])
 def category_detection(request):
     if request.method == "POST":
-        # response
         return category_serializer(request)
-    # render the html
     return render(request , 'projects/category-detection.html')
 
 # requirement
 @require_http_methods(['GET', 'POST'])
 def poet(request):
     if request.method == "POST":
-        # response
         return poetry_creator(request)
-    # render the html
     return render(request, 'projects/poet.html')
 
 
