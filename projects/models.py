02e1609b41b31d1f75592f6bf4665e8578d934f9
aghabidareh <aghabidareh@gmail.com>
2025-03-08 05:03:07 -0800

    add-slug-field

--- projects/models.py
+++ projects/models.py
@@ -8,7 +8,7 @@ class News(models.Model):
 
     image = models.ImageField(upload_to='news/', blank=True, null=True)
 
-    slug = models.SlugField(unique=True , max_length=64)
+    slug = models.SlugField(unique=True , max_length=64 , null=True , blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
