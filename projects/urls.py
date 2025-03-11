ce6aa3f70e19df23f88569f3ca9245c19180e416
aghabidareh <aghabidareh@gmail.com>
2025-03-11 07:29:25 -0700

    cleaning

--- projects/urls.py
+++ projects/urls.py
@@ -3,29 +3,29 @@ from projects.views import *
 
 urlpatterns = [
     # index page
-    path('' , index , name='index'),
+    path('', index, name='index'),
 
     # about me page
-    path('about-me' , about_me , name='about-me'),
+    path('about-me', about_me, name='about-me'),
 
     # why page
-    path('why' , why , name='why'),
+    path('why', why, name='why'),
 
     # similarity between two textx project page
-    path('project/text-similarity' , text_similarity , name='text-similarity'),
+    path('project/text-similarity', text_similarity, name='text-similarity'),
 
     # persian text category detection project page
-    path('project/category_detection' , category_detection , name='category_detection'),
+    path('project/category_detection', category_detection, name='category_detection'),
 
     # poetry project page
-    path('project/poet' , poet , name='poet'),
+    path('project/poet', poet, name='poet'),
 
     # news page
-    path('news' , news , name='news'),
+    path('news', news, name='news'),
 
     # the news page
-    path('news/<int:pk>' , news_page , name='news-page'),
+    path('news/<int:pk>', news_page, name='news-page'),
 
     # the news page
-    path('news/<slug:slug>' , news_page , name='news-page'),
+    path('news/<slug:slug>', news_page, name='news-page'),
 ]
