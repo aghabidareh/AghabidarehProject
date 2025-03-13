f7d207eb213c1f716a472b1d82529c23d9bc53bf
aghabidareh <aghabidareh@gmail.com>
2025-03-13 07:43:01 -0700

    cleaning

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from projects.models import News
 
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_published', 'created_at', 'updated_at')
