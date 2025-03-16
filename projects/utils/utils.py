dad0fa8f5a3de4caaa936dcba16fed4b523474eb
aghabidareh <aghabidareh@gmail.com>
2025-03-16 04:11:25 -0700

    cleaner-code

--- projects/utils/utils.py
+++ projects/utils/utils.py
@@ -6,48 +6,49 @@ from projects.modules.text_similarity.predict import calculate_similarity
 from projects.modules.category_detection.predict import predict
 from projects.modules.poetry.generator import generator
 
+class Helpers:
+    @staticmethod
+    def similarity_detection(request):
+        # process the inputs
+        text1 = request.POST['text1']
+        text2 = request.POST['text2']
 
-def similarity_detection(request):
-    # process the inputs
-    text1 = request.POST['text1']
-    text2 = request.POST['text2']
+        # prepare the response
+        result = calculate_similarity(text1, text2)
 
-    # prepare the response
-    result = calculate_similarity(text1, text2)
+        # prepare the context
+        context = {'result': result}
 
-    # prepare the context
-    context = {'result': result}
+        # send the context
+        return render(request, 'projects/text-similarity.html', context=context)
 
-    # send the context
-    return render(request, 'projects/text-similarity.html', context=context)
+    @staticmethod
+    def category_serializer(request):
+        # prepare the input
+        text = request.POST['text']
 
+        # prepare the response
+        result = predict(text)
 
-def category_serializer(request):
-    # prepare the input
-    text = request.POST['text']
+        # prepare the context
+        context = {'result': result}
 
-    # prepare the response
-    result = predict(text)
+        # send the context
+        return render(request, 'projects/category-detection.html', context)
 
-    # prepare the context
-    context = {'result': result}
+    @staticmethod
+    def poetry_creator(request):
+        # prepare the input
+        topic = request.POST['topic']
 
-    # send the context
-    return render(request, 'projects/category-detection.html', context)
+        # prepare the response
+        result = generator(topic)
 
+        # prepare the context
+        context = {'result': result}
 
-def poetry_creator(request):
-    # prepare the input
-    topic = request.POST['topic']
-
-    # prepare the response
-    result = generator(topic)
-
-    # prepare the context
-    context = {'result': result}
-
-    # send the context
-    return render(request, 'projects/poet.html', context)
+        # send the context
+        return render(request, 'projects/poet.html', context)
 
 class Views:
     def text_similarity_view(self , request):
